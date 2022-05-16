39b76e7f9c229669863d142e707b1f6cf843c288
Frederick Lopez <mtester010@gmail.com>
2022-05-16 13:18:56 -0500

    Add repository to my local

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [users, setUsers] = useState([]);
+  // const [counter2, setCounter2] = useState(0);
+  console.log(counter, "Right in the function");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) => setUsers(user));
+
+    // You can add a return for a cleanUp function from the last time useEffect runs
+    // remove eventlistener to add again
+    return () => {
+      console.log("the return runs");
+      setUsers([]);
+    };
+  }, [counter]);
+  //First time it runs, (MOUNTING)
+  // useEffect has argument, call back function and a array of dependencies as 2nd argument
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      {/* <h1>{counter2}</h1>
+      <button onClick={() => setCounter2(counter2 + 5)}>Increment</button> */}
+      <ul>
+        {users.map((user) => (
+          <li>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
